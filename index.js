556494feebba5d9ea6197ad7e8d96c646898096f
Farman Anees <farmananees831@gmail.com>
2025-04-27 00:27:17 +0500

    g

--- index.js
+++ index.js
@@ -1,39 +1,22 @@
-function weather(){
+function weather() {
+  let city = document.getElementById("serch").value.toLowerCase();
+  let results = document.getElementById("results");
 
-    let city = document.getElementById("serch").value.toLowerCase();
-    let results = document.getElementById("results")
+  // console.log(city,result)
 
-
-    // console.log(city,result)
-
-
-
-    if (city === "lahore")
-    {
-        results.innerHTML ="Today Lahore weather is 35 degree"
-    }
-    else if (city == "karachi")
-    {
-       results.innerHTML ="Today karachi Weather is 45 degree"
-    }
-    else if (city == "Islamabad")
-    {
-       results.innerHTML ="Today Islamabad Weather is 26 degree"
-    }
-    else if (city == "Peshaware")
-    {
-        results.innerHTML ="Today Peshawar weather is 30 degree"
-    }
-    else if (city == "Rwalpindi")
-    {
-       results.innerHTML ="Today Rawalpindi weather is 28 degree"
-    }
-    else if (city == "Faisalabad")
-    {
-       results.innerHTML ="Today Islamabad weather is 26 degree"
-    }
-    else
-    {
-        results.innerHTML ="You Enter Invalid City please Try agian"
-    }    
-    }   
+  if (city == "lahore") {
+    results.innerHTML = "Today Lahore weather is 35 degree";
+  } else if (city == "karachi") {
+    results.innerHTML = "Today karachi Weather is 45 degree";
+  } else if (city == "Islamabad") {
+    results.innerHTML = "Today Islamabad Weather is 26 degree";
+  } else if (city == "Peshaware") {
+    results.innerHTML = "Today Peshawar weather is 30 degree";
+  } else if (city == "Rwalpindi") {
+    results.innerHTML = "Today Rawalpindi weather is 28 degree";
+  } else if (city == "Faisalabad") {
+    results.innerHTML = "Today Islamabad weather is 26 degree";
+  } else {
+    results.innerHTML = "You Enter Invalid City please Try agian";
+  }
+};
